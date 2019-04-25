9e294f599eacc80b844f07855fd1f8843db523dc
Fredrik Gustafon <fredrikgustafson@MacBook-Pro-3.local>
2019-04-25 15:06:24 +0200

    more quotes

--- server.js
+++ server.js
@@ -6,7 +6,16 @@ var quotes = ["It is what is",
             "As we all know",
             "Waste of the money",
             "Royal family - they have Air Planes",
-            "One might think so"];
+            "One might think so",
+            "Bare in mind", 
+            "This is all i can tell you right now",
+            "That's about it",
+            "Let me put it like this ...", 
+            "This is Slavic way of doing business",
+            "Lets put it this way",
+            "I will say this only once", 
+            "This is not the time and place to talk about it",
+            "u can always dance on yacht in wheelchair"];
 
 function newQuote (){
     var randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
